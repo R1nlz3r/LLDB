b0a85c0fa6027117341d884ebeadc67ed6d547a4
Natacha OZANNE <nozanne@e2r2p4.42.fr>
2017-04-30 17:19:51 +0200

    upper and lower

--- 02-script/lowercase.py
+++
@@ -1,9 +0,0 @@
-import lldb
-
-def print_lowercase(debugger, command, result, dict):
-	target = debugger.GetSelectedTarget()
-	if target:
-		print 'FT_' + str(target).lower()
-
-def __lldb_init_module(debugger, internal_dict):
-	debugger.HandleCommand('command script add -f lowercase.print_lowercase lowercase')
--- 02-script/uppercase.py
+++
@@ -1,9 +0,0 @@
-import lldb
-
-def print_uppercase(debugger, command, result, dict):
-	target = debugger.GetSelectedTarget()
-	if target:
-		print 'FT_' + str(target).upper()
-
-def __lldb_init_module(debugger, internal_dict):
-	debugger.HandleCommand('command script add -f uppercase.print_uppercase uppercase')
