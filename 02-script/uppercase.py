011631eb1e9c8bd5afbbbb1c132af842747ded59
Mathis Pandelé <mapandel@student.42.fr>
2017-04-30 10:04:21 +0200

    uppercase.py 1.1 : Fix

--- 02-script/uppercase.py
+++ 02-script/uppercase.py
@@ -2,7 +2,8 @@ import lldb
 
 def print_uppercase(debugger, command, result, dict):
 	target = debugger.GetSelectedTarget()
-	print 'FT_' + str(target).upper()
+	if target:
+		print 'FT_' + str(target).upper()
 
 def __lldb_init_module(debugger, internal_dict):
 	debugger.HandleCommand('command script add -f uppercase.print_uppercase uppercase')
