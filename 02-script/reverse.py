9d0e40eb33fe8f68115bc3d88df98d3c68d06087
Mathis Pandelé <mapandel@student.42.fr>
2017-04-30 08:03:23 +0200

    reverse.py 1.0 : Implementation

---
+++ 02-script/reverse.py
@@ -0,0 +1,8 @@
+import lldb
+
+def print_reverse(debugger, command, result, dict):
+	target = debugger.GetSelectedTarget()
+	print 'FT_' + str(target)[::-1]
+
+def __lldb_init_module(debugger, internal_dict):
+	debugger.HandleCommand('command script add -f reverse.print_reverse reverse')
