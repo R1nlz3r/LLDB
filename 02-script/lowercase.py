fdb492ae5447c283b3db004ca6ef69b9043f18e5
Mathis Pandelé <mapandel@student.42.fr>
2017-04-30 10:04:00 +0200

    lowercase.py 1.1 : Fix

--- 02-script/lowercase.py
+++ 02-script/lowercase.py
@@ -2,7 +2,8 @@ import lldb
 
 def print_lowercase(debugger, command, result, dict):
 	target = debugger.GetSelectedTarget()
-	print 'FT_' + str(target).lower()
+	if target:
+		print 'FT_' + str(target).lower()
 
 def __lldb_init_module(debugger, internal_dict):
 	debugger.HandleCommand('command script add -f lowercase.print_lowercase lowercase')
